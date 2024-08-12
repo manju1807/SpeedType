50eb038bca47783b9f054004883cce51951078a5
manju1807 <rmanjunath18@outlook.com>
2024-08-12 17:33:06 +0530

    SEO updated

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -7,12 +7,12 @@ import Link from 'next/link';
 const Navbar = () => {
   return (
     <div className='container p-6 flex flex-row justify-between items-center'>
-      <div className='flex flex-row items-center justify-center'>
-        <Link href={'/'}>
+      <Link href={'/'}>
+        <div className='flex flex-row items-center justify-center'>
           <Image src={NavImg.src} alt='speedType' height={30} width={30} className='size-8 mr-2' />
           <h1 className='text-2xl font-bold antialiased align-middle tracking-tight'>SpeedType</h1>
-        </Link>
-      </div>
+        </div>
+      </Link>
       <ModeToggle />
     </div>
   )
